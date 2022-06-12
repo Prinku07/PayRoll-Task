3a03890258594aafb4688ceef4c07697d6f81adc
Prinku07 <priyankagohil10@gmail.com>
2022-06-12 20:13:59 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // component
 import { AppComponent } from './app.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 //module
 import { AuthModule } from './Views/Auth/auth.module';
 import { MyTaskModule } from './Views/my-task/my-task.module';
 import { ShareModule } from './share/share.module';
 import { CoreModule } from './core/core.module';
-import { LoaderService } from './core/share/loader.service';
-import { NgHttpLoaderModule } from 'ng-http-loader';
 
 //Toaster service
 @NgModule({
@@ -28,13 +25,8 @@ import { NgHttpLoaderModule } from 'ng-http-loader';
   ShareModule,
   CoreModule,
   ToastrModule.forRoot(),
-  NgHttpLoaderModule.forRoot(),
   AppRoutingModule,
   ],
-  providers: [
-    LoaderService
-  ],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
